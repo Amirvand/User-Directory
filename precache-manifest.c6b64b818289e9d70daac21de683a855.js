dd1c88c6d4c429b1039acd620e384914a42144ba
Amir <aamirvand97@gmail.com>
2019-12-03 20:29:29 -0800

    Updates

---
+++ precache-manifest.c6b64b818289e9d70daac21de683a855.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4abce98b50c8c37b86f8fa84028f9d29",
+    "url": "/Amirvand/Employee-Managment/tree/gh-pages/index.html"
+  },
+  {
+    "revision": "d1f6a31ce416939411b9",
+    "url": "/Amirvand/Employee-Managment/tree/gh-pages/static/css/main.42a301b0.chunk.css"
+  },
+  {
+    "revision": "82b7557759b51cb27cf8",
+    "url": "/Amirvand/Employee-Managment/tree/gh-pages/static/js/2.bfbb5337.chunk.js"
+  },
+  {
+    "revision": "d1f6a31ce416939411b9",
+    "url": "/Amirvand/Employee-Managment/tree/gh-pages/static/js/main.edf27c3c.chunk.js"
+  },
+  {
+    "revision": "db730b46b3c93f089e54",
+    "url": "/Amirvand/Employee-Managment/tree/gh-pages/static/js/runtime~main.488c5762.js"
+  }
+]);
